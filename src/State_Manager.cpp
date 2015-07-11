427527812eaa3e49ca7ebb6ccbcfaffb38430c9e
FlynnsPixel <flynnspixel@gmail.com>
2015-07-12 03:45:03 +1000

    Basic winsock connection to server

--- src/State_Manager.cpp
+++ src/State_Manager.cpp
@@ -1,5 +1,12 @@
 #include "State_Manager.h"
 #include <time.h>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+
+// Need to link with Ws2_32.lib, Mswsock.lib, and Advapi32.lib
+#pragma comment(lib, "Ws2_32.lib")
+#pragma comment(lib, "Mswsock.lib")
+#pragma comment(lib, "AdvApi32.lib")
 
 using namespace cocos2d;
 
@@ -25,6 +32,47 @@ bool State_Manager::init() {
 
 	scheduleUpdate();
 	init_time = clock();
+
+	WSAData wsa_data;
+	SOCKET conn_sock = INVALID_SOCKET;
+	struct addrinfo* result, hints;
+	int err;
+	char* ip = "104.236.253.123";
+	char* port = "4222";
+
+	if ((err = WSAStartup(MAKEWORD(2, 2), &wsa_data)) != 0) {
+		CCLOG("WSA Startup failed! Err: %d", err);
+	}
+
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+
+	if ((err = getaddrinfo(ip, port, &hints, &result)) != 0) {
+		CCLOG("getaddrinfo failed (ip: %s, port: %s). Err %d", ip, port, err);
+	}
+
+	while (result != NULL) {
+		if ((conn_sock = socket(result->ai_family, result->ai_socktype, result->ai_protocol)) == INVALID_SOCKET) {
+			CCLOG("connection socket failed with error: %d", err);
+			break;
+		}
+		if ((err = connect(conn_sock, result->ai_addr, (int)result->ai_addrlen)) == SOCKET_ERROR) {
+			closesocket(conn_sock);
+			conn_sock = INVALID_SOCKET;
+			continue;
+		}
+		result = result->ai_next;
+	}
+	freeaddrinfo(result);
+
+	if (conn_sock == INVALID_SOCKET) {
+		CCLOG("could not connect to server. err: %d", err);
+	}
+
+	char* buffer = "ayy lmao";
+	send(conn_sock, buffer, strlen(buffer), 0);
 
     return true;
 }
