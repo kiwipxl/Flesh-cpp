b1fc779129cf6070e9b01d97f8614bcdca41e35c
FlynnsPixel <flynnspixel@gmail.com>
2015-07-28 02:49:19 +1000

    level kinda loads

--- proj.win32/src/StateManager.cpp
+++ proj.win32/src/StateManager.cpp
@@ -16,30 +16,11 @@ cc::TextFieldDelegate username_input;
 
 cc::Sprite* state::player;
 cc::DrawNode* d_node;
-std::vector<cc::Vec2> points;
-std::vector<cc::Vec2> uvs;
-std::vector<cc::Color4F> colours;
+std::vector<cc::V3F_C4B_T2F> points;
 
-cc::CustomCommand* custom_cmd;
-cc::PrimitiveCommand p_cmd;
-
-cc::V3F_C4B_T2F* verts;
 cc::TrianglesCommand tri_cmd;
 cc::TrianglesCommand::Triangles tri;
-//unsigned short* indices;
-
-cc::VertexBuffer* vertexBuffer;
-cc::VertexData* vertexData;
-cc::IndexBuffer* indexBuffer;
-short* indices;
-cc::Primitive* primitive;
-
-cc::VertexStreamAttribute* pos_vs;
-cc::VertexStreamAttribute* col_vs;
-cc::VertexStreamAttribute* tex_vs;
-
-cc::QuadCommand* qcmd;
-cc::V3F_C4B_T2F_Quad pq;
+u_short* indices;
 
 void create_state(State c_state) {
     using namespace state;
@@ -76,39 +57,6 @@ void state::switch_state(State new_state) {
     create_state(new_state);
 }
 
-void state::on_draw() {
-    //cc::GLProgram* s = cc::GLProgram::createWithFilenames("basic.vert", "basic.frag");
-    //scene->setGLProgram(cc::ShaderCache::getInstance()->getGLProgram(cc::GLProgram::SHADER_NAME_POSITION_COLOR));
-
-    float gradientAlpha = 0.7;
-    cc::Vec2 vertices[4];
-    cc::Color4F colors[4];
-    int nVertices = 0;
-
-    vertices[nVertices] = cc::Vec2(0, 0);
-    colors[nVertices++] = { 0, 1, 1, 0 };
-    vertices[nVertices] = cc::Vec2(0, 0);
-    colors[nVertices++] = { 1, 1, 0, 0 };
-    vertices[nVertices] = cc::Vec2(0, 0);
-    colors[nVertices++] = { 0, 1, 0, gradientAlpha };
-    vertices[nVertices] = cc::Vec2(0, 0);
-    colors[nVertices++] = { 0, 1, 0, gradientAlpha };
-
-    //cc::ccGLBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-    //cc::ccGLBindTexture2D(player->getTexture()->getName());
-    //cc::ccGLEnableVertexAttribs(cc::GL::VERTEX_ATTRIB_FLAG_POSITION | cc::GL::VERTEX_ATTRIB_FLAG_COLOR);
-
-    glClearColor((cos(scene->time_since_startup * 4.0f) / 2.0f) + .5f, 0, 0, 1);
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    glBegin(GL_TRIANGLES);
-    glColor4f(1, 1, 1, 1);
-    glVertex3f(0, 0, 0);
-    glVertex3f(40, 0, 0);
-    glVertex3f(0, 40, 0);
-    glEnd();
-}
-
 void state::init(SceneManager* scene_ref) {
     scene = scene_ref;
     scene->setGLProgram(cc::ShaderCache::getInstance()->getGLProgram(cc::GLProgram::SHADER_NAME_POSITION_COLOR));
@@ -117,8 +65,6 @@ void state::init(SceneManager* scene_ref) {
     input::init();
 
     create_state(s);
-
-    custom_cmd = new cc::CustomCommand();
 
     FILE* f = fopen("terrain.t2d", "r");
 
@@ -131,35 +77,26 @@ void state::init(SceneManager* scene_ref) {
         float v_y = 0;
         std::string temp = "";
         bool s = false;
-        for (int n = 0; n < max_buf; ++n) {
+        for (int n = 12; n < max_buf; ++n) {
             char c = buf[n];
             if (c == '\0') break;
             if (c == ',') {
                 if (s) {
                     v_y = std::stof(temp);
-                    cc::Vec2 v;
-                    v.x = v_x * 40.0f;
-                    v.y = v_y * 40.0f;
-                    v.x += 400;
-                    v.y += 100;
+                    cc::V3F_C4B_T2F v;
+                    v.vertices.x = v_x * 100.0f;
+                    v.vertices.y = v_y * 100.0f;
+                    v.colors = cc::Color4B(255, 255, 255, 255);
+                    v.texCoords.u = 0;
+                    v.texCoords.v = 0;
                     points.push_back(v);
 
-                    cc::Vec2 uv;
-                    uv.x = 0;
-                    uv.y = 1;
-                    uvs.push_back(uv);
-
-                    cc::Color4F c;
-                    c.r = 1.0f;
-                    c.g = 0.0f;
-                    c.b = 1.0f;
-                    c.a = 1.0f;
-                    colours.push_back(c);
-
-                    //if (points.size() >= 6) break;
+                    //if (points.size() >= 12) break;
                 }else v_x = std::stof(temp);
                 temp = "";
                 s = !s;
+            }else if (c == ' ') {
+                break;
             }else {
                 temp += c;
             }
@@ -179,76 +116,25 @@ void state::init(SceneManager* scene_ref) {
     player->setPhysicsBody(physics_body);
 
     scene->p_world->setDebugDrawMask(cc::PhysicsWorld::DEBUGDRAW_ALL);
-    auto dphysics_body = cc::PhysicsBody::create();
-    //dphysics_body->addShape(cc::PhysicsShapePolygon::create(&points[0], points.size()));
 
     d_node = cc::DrawNode::create();
+    //dphysics_body->setGravityEnable(false);
+    //dphysics_body->setDynamic(false);
+    scene->addChild(d_node, 1);
 
-    d_node->setPhysicsBody(dphysics_body);
-    dphysics_body->setGravityEnable(false);
-    dphysics_body->setDynamic(false);
-    //scene->addChild(d_node, 1);
-    
-    verts = new cc::V3F_C4B_T2F[4];
-    verts[0].vertices = cc::Vertex3F(0, 0, 0);
-    verts[0].colors = cc::Color4B(255, 255, 0, 255);
-    verts[0].texCoords = cc::Tex2F(0, 0);
+    indices = new u_short[points.size()] { 0,1,2,3,1,0,3,4,1,3,5,4,3,6,5,3,7,6,7,3,8,9,10,11,12,9,11,13,14,14,13,13,14,15,16,16,15,15,16,17,18,19,20,17,19,21,22,22,21,21,22,23,24,24,23,23,24,25,26,27,28,25,27,29,30,31,32,29,31,33,34,35,36,33,35,37,38,39,40,37,39,41,42,43,44,41,43,45,46,46,45,45,46,47,48,48,47,47,48,49,50,51,52,49,51,53,54,55,56,53,55,57,58,59,60,57,59,61,62,63,64,61,63,65,66,67,68,65,67,69,70,71,72,69,71,73,74,75,76,73,75,77,78,79,80,77,79,81,82,83,84,81,83, };
 
-    verts[1].vertices = cc::Vertex3F(400, 0, 0);
-    verts[1].colors = cc::Color4B(255, 255, 0, 255);
-    verts[1].texCoords = cc::Tex2F(0, 0);
-
-    verts[2].vertices = cc::Vertex3F(0, 400, 0);
-    verts[2].colors = cc::Color4B(255, 255, 0, 255);
-    verts[2].texCoords = cc::Tex2F(0, 0);
-
-    verts[0] = player->getQuad().tl;
-    verts[1] = player->getQuad().tr;
-    verts[2] = player->getQuad().br;
-    verts[3] = player->getQuad().bl;
-
-    indices = new short[] {0, 1, 2};
-    tri.indices = (unsigned short*)indices;
-    tri.indexCount = 3;
-    tri.verts = verts;
-    tri.vertCount = 3;
-
-    vertexBuffer = cc::VertexBuffer::create(sizeof(cc::V3F_C4B_T2F), 3);
-    vertexBuffer->retain();
-    vertexBuffer->updateVertices(&verts, 3, 0);
-
-    vertexData = cc::VertexData::create();
-    vertexData->retain();
-    vertexData->setStream(vertexBuffer, cc::VertexStreamAttribute(0, cc::GLProgram::VERTEX_ATTRIB_POSITION, GL_FLOAT, 3, false));
-    vertexData->setStream(vertexBuffer, cc::VertexStreamAttribute(12, cc::GLProgram::VERTEX_ATTRIB_COLOR, GL_UNSIGNED_BYTE, 4, true));
-    vertexData->setStream(vertexBuffer, cc::VertexStreamAttribute(16, cc::GLProgram::VERTEX_ATTRIB_TEX_COORD, GL_FLOAT, 2, false));
-
-    indexBuffer = cc::IndexBuffer::create(cc::IndexBuffer::IndexType::INDEX_TYPE_SHORT_16, 3);
-    indexBuffer->retain();
-    indexBuffer->updateIndices((unsigned short*)indices, 3, 0);
-
-    primitive = cc::Primitive::create(vertexData, NULL, GL_TRIANGLES);
-    primitive->retain();
-    primitive->setStart(0);
-    primitive->setCount(3);
-
-    p_cmd.init(0.0f, player->getTexture()->getName(), player->getGLProgramState(),
-        player->getBlendFunc(), primitive, player->getNodeToWorldTransform(), 1);
-    cc::Director::getInstance()->getRenderer()->addCommand(&p_cmd);
+    tri.indices = indices;
+    tri.indexCount = points.size();
+    tri.verts = &points[0];
+    tri.vertCount = points.size();
 
     tri_cmd.init(0.0f, player->getTexture()->getName(), player->getGLProgramState(), 
         player->getBlendFunc(), tri, player->getNodeToWorldTransform(), 1);
-
-    //player->draw(cc::Director::getInstance()->getRenderer(), player->getNodeToWorldTransform(), 1);
-
-    //cc::Director::getInstance()->getRenderer()->addCommand(&tri_cmd);
 }
 
 void state::update(float dt) {
-    p_cmd.init(0.0f, player->getTexture()->getName(), player->getGLProgramState(),
-               player->getBlendFunc(), primitive, player->getNodeToWorldTransform(), 1);
-    cc::Director::getInstance()->getRenderer()->addCommand(&p_cmd);
-    int dv = cc::Director::getInstance()->getRenderer()->getDrawnVertices();
+    cc::Director::getInstance()->getRenderer()->addCommand(&tri_cmd);
 
     player->getPhysicsBody()->applyForce(cc::Vec2(0, -2000.0f));
     if (input::key_down(cc::EventKeyboard::KeyCode::KEY_RIGHT_ARROW)) {
