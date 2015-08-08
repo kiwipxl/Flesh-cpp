ca3c385f7fe411eb7932e0dd5d50a25e36bfd270
FlynnsPixel <flynnspixel@gmail.com>
2015-08-08 12:33:36 +1000

    ferr2d split tokens function and return error macro

--- proj.win32/src/map/Ferr2DSystem.cpp
+++ proj.win32/src/map/Ferr2DSystem.cpp
@@ -91,24 +91,31 @@ namespace map {
             f_assert(sstream << "ferr2d load error occurred: " << err_message << " (" << file_name << ")");
         }
 
-        bool split_tokens(std::string& data, std::vector<std::string>& tokens, std::string attrib_name) {
+        bool split_tokens(std::string& data, std::vector<std::string>& tokens, std::string attrib_name, char delimiter = ',') {
             int index = -1;
             int nl_index = -1;
             int co_index = -1;
             if ((index = data.find(attrib_name)) != -1 && 
                 (nl_index = data.substr(index).find('\n')) != -1 && 
                 (co_index = (data.substr(index).find(':') + 1)) != -1) {
-                tokens = split(data.substr(index + co_index, nl_index - co_index), ',');
+                tokens = split(data.substr(index + co_index, nl_index - co_index), delimiter);
                 return true;
             }
             tokens.clear();
             return false;
         }
 
+#define RETURN_LOAD_ERR(err_message, file_name)                 \
+        if (temp != NULL) delete[] temp;                        \
+        print_load_error(err_message, file_name);               \
+        fclose(f);                                              \
+        return NULL;
+
         TerrainData* load(std::string file_name) {
 	        TerrainData* ter = new TerrainData();
 	
-	        FILE* f = fopen(file_name.c_str(), "r");
+            FILE* f = fopen(file_name.c_str(), "r");
+            char* temp = NULL;
 
             if (f != NULL) {
                 fseek(f, 0, SEEK_END);
@@ -116,14 +123,14 @@ namespace map {
                 rewind(f);
 
                 if (file_len <= 0) { print_load_error("file length cannot be less than 0", file_name); return NULL; }
-                char* temp = new char[file_len];
+                temp = new char[file_len];
                 fread(temp, 1, file_len, f);
 
                 ter->debug_draw_node = DrawNode::create();
 
                 std::string data = temp;
                 std::vector<std::string> tokens;
-		        if (split_tokens(data, tokens, "vertex_data2:")) {
+		        if (split_tokens(data, tokens, "vertex_data:")) {
 			        for (int n = 0; n < tokens.size(); n += 2) {
                         V3F_C4B_T2F v;
 				        v.vertices.x = std::stof(tokens[n]) * 40.0f;
@@ -137,9 +144,7 @@ namespace map {
 				        ter->debug_points.push_back(dv);
 			        }
 		        }else {
-			        delete[] temp;
-			        print_load_error("vertex_data attribute missing", file_name);
-			        return NULL;
+                    RETURN_LOAD_ERR("vertex_data attribute missing", file_name);
                 }
                 if (split_tokens(data, tokens, "indices:")) {
                     for (int n = 0; n < tokens.size(); ++n) {
@@ -154,9 +159,7 @@ namespace map {
                         }
                     }
 		        }else {
-			        delete[] temp;
-			        print_load_error("indices attribute missing", file_name);
-			        return NULL;
+                    RETURN_LOAD_ERR("indices attribute missing", file_name);
                 }
                 if (split_tokens(data, tokens, "uvs:")) {
                     for (int n = 0; n < tokens.size(); n += 2) {
@@ -165,9 +168,7 @@ namespace map {
                         v.texCoords.v = 1 - std::stof(tokens[n + 1]);
                     }
 		        }else {
-			        delete[] temp;
-			        print_load_error("uvs attribute missing", file_name);
-			        return NULL;
+                    RETURN_LOAD_ERR("uvs attribute missing", file_name);
                 }
                 if (split_tokens(data, tokens, "collider_points:")) {
                     for (int n = 0; n < tokens.size(); n += 2) {
@@ -181,30 +182,29 @@ namespace map {
                         }
                     }
 		        }else {
-			        delete[] temp;
-			        print_load_error("colider_points attribute missing", file_name);
-			        return NULL;
+                    RETURN_LOAD_ERR("colider_points attribute missing", file_name);
                 }
-                if (split_tokens(data, tokens, "edge_indices:")) {
+                if (split_tokens(data, tokens, "edge_indices:", '-')) {
                     if (tokens.size() >= 2) {
-				        ter->set_edge_index_attrib(std::stof(tokens[0]), std::stof(tokens[1]));
+                        ter->set_edge_index_attrib(std::stof(tokens[0]), std::stof(tokens[1]));
+                    }else {
+                        RETURN_LOAD_ERR("edge_indices attribute expects more than 2 tokens", file_name);
                     }
 		        }else {
-			        print_load_error("edge_indices attribute missing", file_name);
-			        return NULL;
+                    RETURN_LOAD_ERR("edge_indices attribute missing", file_name);
                 }
-                if (split_tokens(data, tokens, "fill_indices:")) {
+                if (split_tokens(data, tokens, "fill_indices:", '-')) {
                     if (tokens.size() >= 2) {
 				        ter->set_fill_index_attrib(std::stof(tokens[0]), std::stof(tokens[1]));
+                    }else {
+                        RETURN_LOAD_ERR("fill_indices attribute expects more than 2 tokens", file_name);
                     }
 		        }else {
-			        delete[] temp;
-			        print_load_error("fill_indices attribute missing", file_name);
-			        return NULL;
+                    RETURN_LOAD_ERR("fill_indices attribute missing", file_name);
                 }
 		        delete[] temp;
 	        }else {
-		        print_load_error("file could not be found", file_name);
+                RETURN_LOAD_ERR("file could not be found", file_name);
             }
 
             fclose(f);
